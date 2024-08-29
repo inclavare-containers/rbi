9d0d83d3cc8e22d19ecb55a8902c59581e5275e4
zny666 <zny666@sjtu.edu.cn>
2024-08-29 18:30:57 +0800

    add docker-based rbi for CDH and AA(changed)

--- confidential-component/attestation-agent/run.sh
+++ confidential-component/attestation-agent/run.sh
@@ -1,23 +1,16 @@
 #! /bin/bash
 
+# set CDH_COMMIT for git checkout
 AA_COMMIT="bf7ccd301d3f50bfcb4cc9e38ae187141ce35072"
-
+# build docker image for CDH
 sudo docker build --progress=plain --no-cache --build-arg AA_COMMIT=${AA_COMMIT} -t rbi-aa:v1 .
-sudo docker build --progress=plain --no-cache --build-arg AA_COMMIT=${AA_COMMIT} -t rbi-aa:v2 .
 
-mkdir -m 755 -p pkg1
-mkdir -m 755 -p pkg2
-
+# run docker container for CDH
 sudo docker run -d --network host --name aa-build1 rbi-aa:v1
-sudo docker run -d --network host --name aa-build2 rbi-aa:v2
 
-sudo docker cp aa-build1:/usr/local/bin/attestation-agent ./pkg1
-sudo docker cp aa-build2:/usr/local/bin/attestation-agent ./pkg2
+# get binary CDH file from image
+sudo docker cp aa-build1:/usr/local/bin/attestation-agent ./
 
-diffoscope ./pkg1/attestation-agent ./pkg2/attestation-agent --html diff.html
-
+# stop and delete container 
 sudo docker stop aa-build1
-sudo docker stop aa-build2
-
 sudo docker rm aa-build1
-sudo docker rm aa-build2
